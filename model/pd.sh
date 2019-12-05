22436ae34258b20e89375bdd133d9de73e13ba7e
catskillsresearch <38082822+catskillsresearch@users.noreply.github.com>
2019-12-05 10:37:29 -0500

    Customizing vanilla solution

---
+++ model/pd.sh
@@ -0,0 +1,5 @@
+python process_data.py \
+       --input_dir /home/catskills/Desktop/dataxv2/xBD \
+       --output_dir /home/catskills/Desktop/dataxv2/classifier \
+       --output_dir_csv /home/catskills/Desktop/dataxv2/classifier/csv \
+       --val_split_pct 0.75
---
+++ model/train.sh
@@ -0,0 +1,9 @@
+python damage_classification.py \
+       --train_data /home/catskills/Desktop/dataxv2/classifier \
+       --train_csv /home/catskills/Desktop/dataxv2/classifier/csv/train.csv \
+       --test_data /home/catskills/Desktop/dataxv2/classifier \
+       --test_csv /home/catskills/Desktop/dataxv2/classifier/csv/test.csv \
+       --model_out /home/catskills/Desktop/dataxv2/model2 \
+       --model_in /home/catskills/Desktop/dataxv2/model-saved-model-100-0.89.hdf5
+
+
