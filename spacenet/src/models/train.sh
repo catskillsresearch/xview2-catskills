90f15fd63f4026bd0312e2ffa62b49bf5342deda
catskillsresearch <38082822+catskillsresearch@users.noreply.github.com>
2019-12-05 10:52:32 -0500

    Merge offline

---
+++ spacenet/src/models/train.sh
@@ -0,0 +1,2 @@
+python train_model.py /home/catskills/Desktop/dataxv2/xBD/spacenet_gt/dataSet/ /home/catskills/Desktop/dataxv2/xBD/spacenet_gt/images/ /home/catskills/Desktop/dataxv2/xBD/spacenet_gt/labels/ -e 100
+es
