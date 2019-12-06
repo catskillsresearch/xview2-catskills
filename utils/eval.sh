48bc5fec6dde879b81cb32939a238724a7e1e03f
Lars Ericson <lars.ericson@catskillsresearch.com>
2019-12-06 08:04:55 -0500

    Gather any changes on first baseline submission

--- utils/eval.sh
+++ utils/eval.sh
@@ -4,11 +4,14 @@ set -x
 
 export DISASTER=santa-rosa-wildfire
 export IMAGEID=00000030
+export MODEL_DIR=/home/catskills/Desktop/dataxv2/release/v_catskills_0.0.0/
+export LOCALIZATION_MODEL=${MODEL_DIR}/localization.hdf5
+export DAMAGE_MODEL=${MODEL_DIR}/classification.hdf5
 
 ./inference.sh \
     -x /home/catskills/Desktop/crcxv2 \
     -i /home/catskills/Desktop/dataxv2/xBD/${DISASTER}/images/${DISASTER}_${IMAGEID}_pre_disaster.png \
     -p /home/catskills/Desktop/dataxv2/xBD/${DISASTER}/images/${DISASTER}_${IMAGEID}_post_disaster.png \
-    -l /home/catskills/Desktop/crcxv2/spacenet/models/logs/model_iter_10700 \
-    -c /home/catskills/Desktop/dataxv2/model2-saved-model-13-0.92.hdf5 \
+    -l ${LOCALIZATION_MODEL} \
+    -c ${DAMAGE_MODEL} \
     -o ~/Desktop/crcxv2/utils/larswe1.png -y
--- utils/prediction.py
+++ utils/prediction.py
@@ -1,6 +1,8 @@
 #!/home/catskills/anaconda3/envs/xview2/bin/python
 
-import shutil, glob
+import glob, os
+from shutil import copyfile
+from tqdm import tqdm
 from subprocess import call
 from IPython.utils.path import ensure_dir_exists
 
@@ -20,12 +22,17 @@ ensure_dir_exists(SUBMIT_DIR)
 
 files = glob.glob(f'{TESTDIR}test_pre_*.png')
 
-for pre_png in tqdim(files):
+for pre_png in tqdm(files):
     post_png = pre_png.replace('_pre_','_post_')
     image_id = pre_png.split('.')[0].split('/')[-1].split('_')[-1]
+    out_damage_png = f'{SUBMIT_DIR}/test_damage_{image_id}_prediction.png'
+    if os.path.isfile(out_damage_png):
+        continue
     out_local_json = f'{SUBMIT_DIR}/test_localization_{image_id}_prediction.json'
     out_local_png = f'{SUBMIT_DIR}/test_localization_{image_id}_prediction.png'
-    out_damage_png = f'{SUBMIT_DIR}/test_damage_{image_id}_prediction.png'
     C=f'./inference.sh -x {CODEDIR} -i {pre_png} -p {post_png} -l {LOCALIZATION_MODEL} -c {DAMAGE_MODEL} -o {out_damage_png} -y'
     call(C, shell=True)
-    shtuil.copyfile(out_damage_png, out_local_png)
+    if os.path.isfile(out_damage_png):
+        copyfile(out_damage_png, out_local_png)
+    else:
+        print("PROCESS FAILED", image_id)
